3d6887554540fe01178baa5f8de5f78d89a5c22d
Abhishek Prasanna Walavalkar <abhishekw13@gmail.com>
2026-07-23 21:26:46 -0500

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        out = []
+        used = set()
+        def dfs(arr):
+
+            nonlocal out
+
+            #exit condn
+
+            if len(arr) == len(nums):
+                out.append(arr.copy())
+                return
+
+            for i in range(len(nums)):
+                if i not in used:
+                    
+                    used.add(i)
+                    arr.append(nums[i])
+                    dfs(arr)
+                    arr.pop()
+                    used.remove(i)
+
+        dfs([])
+
+        return out
