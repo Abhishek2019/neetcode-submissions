f869e857a88bfb578b62c2da682ef773d7bf397a
Abhishek Prasanna Walavalkar <abhishekw13@gmail.com>
2026-07-30 18:54:34 -0500

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        out_map = {}
+        for str in strs:
+
+            temp_set = frozenset(Counter(str).items())
+            print(temp_set)
+
+            if temp_set not in out_map:
+                out_map[temp_set] = [str]
+
+            else:
+                out_map[temp_set].append(str)
+
+
+        return [val for val in out_map.values()]
+
+            
